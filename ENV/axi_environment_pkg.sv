daee8aa3bd8a2072ea384b6aca5c997d1a8d40e2
MohamedRazeetha22 <mohamedrazeetha22@gmail.com>
2026-01-31 11:03:20 +0530

    Create axi_environment_pkg.sv

---
+++ ENV/axi_environment_pkg.sv
@@ -0,0 +1,11 @@
+`ifndef AXI_ENVIRONMENT_PKG
+`define AXI_ENVIRONMENT_PKG
+package axi_environment_pkg;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+import axi_agent_pkg::*;
+`include "axi_scoreboard.sv"
+//`include "axi_coverage.sv"
+`include "axi_environment.sv"
+endpackage
+`endif
